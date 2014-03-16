bde54caeab0e2cf8d84f0d4a6841b54918b7fbc5
kwokhou <kwokhou@gmail.com>
2014-03-17 06:44:28 +0800

    Testing mongodb backend storage

--- routes/index.js
+++ routes/index.js
@@ -26,13 +26,41 @@ exports.home = function (req, res){
   res.render('home', {title: 'Home' });
 };
 
+var mongo = require('mongoose');
+mongo.connect('mongodb://localhost:port/db_name');
+var db = mongo.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function cb() {
+    console.log('mongodb connection is opened.');
+});
+
+var Account = mongo.model('Account', {
+    name: String,
+    last4digit: String,
+    amount: Number,
+    date: { type: Date, default: Date.now }
+});
+
 exports.networth = {
+
   get: function (req, res) {
     console.log('networth get');
     res.render('networth', {title: 'Net worth' });
   },
   post: function (req, res) {
-    console.log('networth post');
+
+    var acc = new Account({
+        name: req.body.name,
+        amount: req.body.amount,
+        last4digit: req.body.last4digit
+    });
+
+    acc.save(function (err) {
+        if (err) {
+            console.log(err);
+        }
+        console.log('Account saved');
+    });
     res.render('networth', {title: 'Net worth' });
   }
 };
