5e16d4713ae0bf6eb35dfdc89502f1a54d88ca1d
Andy Yong <kwokhou@gmail.com>
2014-04-02 23:07:35 +0800

    Make angular working again

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,14 +1,37 @@
-var miriApp = angular.module('miriApp', ['ngRoute']);
+var app = angular.module('myApp', []);
 
-miriApp.controller('MiriListCtrl', ['$scope', function ($scope) {
-    $scope.phones = [
-        {
-            'name': 'Nexus S',
-            'snippet': 'fast just got faster with Nexus S'
-        },
-        {
-            'name': 'Motorola XOOM',
-            'snippet': 'The next, next generation tablet'
+app.service('nametrickService', function () {
+    this.reverse = function (name) {
+        return name.split("").reverse().join("");
+    };
+});
+
+app.factory('nametrickFactory', function () {
+    return {
+        reverse: function (name) {
+            return name.split("").reverse().join("");
         }
-    ];
-}]);
+    };
+});
+
+app.controller('AppCtrl', function AppCtrl($scope, nametrickService, nametrickFactory) {
+    $scope.name = 'Guest';
+
+    $scope.reverseNameService = function () {
+        $scope.name = nametrickService.reverse($scope.name);
+    };
+
+    $scope.reverseNameFactory = function () {
+        $scope.name = nametrickFactory.reverse($scope.name);
+    };
+
+    $scope.projects = [{
+        pid: '9dfjalskdjfa98ausdfalskd0',
+        name: 'My Personal Budget',
+        desc: 'An app that keep tracks of my personal budget and spending'
+    }, {
+        pid: 'jdfjalskdjfa98ausdfajj334',
+        name: 'Miri Now',
+        desc: 'A Personal Assistance app that keep tracks all my TODO stuff'
+    }];
+});
